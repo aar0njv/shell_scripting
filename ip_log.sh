7670136a7079925cb30fe8c5fb40717ba1eacc55
Aaron Joy <aaronjoy382@gmail.com>
2026-07-07 14:24:05 +0530

    assignment 4(2)

---
+++ ip_log.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <log_file>" >&2
+	exit 1
+fi 
+
+LOG_FILE="$1"
+
+read -p "Enter the threshold: " THRESHOLD
+
+if [  ! -f "$LOG_FILE" ]; then
+	echo "Error: File doesn't exist." >&2
+	exit 1
+fi
+
+
+awk '{print $1}' "$LOG_FILE" |  sort | uniq -c | while read -r COUNT IP
+do 
+	if [ "$COUNT" -gt "$THRESHOLD" ]; then
+		echo "IP: $IP -> Total Requests: $COUNT"
+	fi
+done
